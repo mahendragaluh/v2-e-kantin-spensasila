d3121b7256ba5a2f589eb949c11cac35da666da5
mahendragaluh <galuhmahendra4@gmail.com>
2023-05-16 22:01:55 +0700

    16/05/23 MID

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,124 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>E-Kantin SMP | Login</title>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+</head>
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+<body class="hold-transition login-page">
+    <div class="login-box">
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+        @if (session('success'))
+            <p class="alert alert-success">{{ session('success') }}</p>
+        @endif
+        @if ($errors->any())
+            @foreach ($errors->all() as $err)
+                <p class="alert alert-danger">{{ $err }}</p>
+            @endforeach
+        @endif
+
+        <div class="login-logo">
+            <a href="#">
+                <b>E-KANTIN</b> SMP NEGERI 1 SILIRAGUNG
+            </a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Masuk sebagai pengguna terdaftar</p>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="input-group form-group mb-3">
+                        <input type="username" name="username" class="form-control" value="{{ old('username') }}"
+                            placeholder="Username" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
                             </div>
-
-                            <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                    <div class="input-group form-group mb-3">
+                        <input id="inputPassword" type="password" name="password" class="form-control"
+                            placeholder="Password" required>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span id="passwordToggle" class="fas fa-eye"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="social-auth-links text-center mb-3">
+                        <button type="submit" class="btn btn-block btn-primary">
+                            Sign In
+                        </button>
+                        <a href="/" class="btn btn-block btn-danger">
+                            Back
+                        </a>
+                    </div>
+                    <!-- /.social-auth-links -->
+                </form>
+
+                <p class="mb-0">
+                    <a href="/register" class="text-center">Belum memiliki akun? Daftar sekarang!</a>
+                </p>
+
+                <p class="mb-1">
+                    <a href="#">Lupa Password?</a>
+                </p>
+
             </div>
+            <!-- /.login-card-body -->
         </div>
     </div>
-@endsection
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('lte/plugins/select2/js/select2.full.min.js') }}"></script>
+    <!-- jquery-validation -->
+    <script src="{{ asset('lte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('lte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('lte/dist/js/adminlte.js') }}"></script>
+
+    <script>
+        let isPasswordVisible = false;
+
+        $('#passwordToggle').on('click', function() {
+            isPasswordVisible = !isPasswordVisible;
+
+            if (isPasswordVisible) {
+                $('#inputPassword').attr('type', 'text');
+                $('#passwordToggle').removeClass('fa-eye');
+                $('#passwordToggle').addClass('fa-eye-slash');
+            } else {
+                $('#inputPassword').attr('type', 'password');
+                $('#passwordToggle').removeClass('fa-eye-slash');
+                $('#passwordToggle').addClass('fa-eye');
+            }
+        });
+    </script>
+
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,155 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>E-Kantin SMP | Register</title>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+</head>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="hold-transition register-page">
+    <div class="register-box">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+        @if ($errors->any())
+            @foreach ($errors->all() as $err)
+                <p class="alert alert-danger">{{ $err }}</p>
+            @endforeach
+        @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="register-logo">
+            <a href="#"><b>E-KANTIN</b> SMP NEGERI 1 SILIRAGUNG</a>
         </div>
+
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Daftar sebagai pengguna baru</p>
+
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}"
+                            placeholder="Full name">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="username" class="form-control" name="username" value="{{ old('username') }}"
+                            placeholder="Username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                            placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="inputPasswordSignup" type="password" class="form-control" name="password"
+                            placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span id="passwordToggleSignup" class="fas fa-eye"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input id="inputConfPasswordSignup" type="password" class="form-control"
+                            name="password_confirmation" placeholder="Retype password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span id="confPasswordToggleSignup" class="fas fa-eye"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="social-auth-links text-center">
+                        <button type="submit" class="btn btn-block btn-primary">
+                            Sign up
+                        </button>
+                        <a href="/" class="btn btn-block btn-danger">
+                            Back
+                        </a>
+                    </div>
+                </form>
+                <a href="/login" class="text-center">Sudah memiliki akun? Sign in sekarang!</a>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
     </div>
-</div>
-@endsection
+    <!-- /.register-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('lte/plugins/select2/js/select2.full.min.js') }}"></script>
+    <!-- jquery-validation -->
+    <script src="{{ asset('lte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('lte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('lte/dist/js/adminlte.js') }}"></script>
+
+    <script>
+        let isPasswordSignupVisible = false;
+        let isConfPasswordSignupVisible = false;
+
+        $('#passwordToggleSignup').on('click', function() {
+            isPasswordSignupVisible = !isPasswordSignupVisible;
+            console.log("password toggle");
+
+            if (isPasswordSignupVisible) {
+                $('#inputPasswordSignup').attr('type', 'text');
+                $('#passwordToggleSignup').removeClass('fa-eye');
+                $('#passwordToggleSignup').addClass('fa-eye-slash');
+            } else {
+                $('#inputPasswordSignup').attr('type', 'password');
+                $('#passwordToggleSignup').removeClass('fa-eye-slash');
+                $('#passwordToggleSignup').addClass('fa-eye');
+            }
+        });
+
+        $('#confPasswordToggleSignup').on('click', function() {
+            isConfPasswordSignupVisible = !isConfPasswordSignupVisible;
+            console.log("conf password toggle");
+
+            if (isConfPasswordSignupVisible) {
+                $('#inputConfPasswordSignup').attr('type', 'text');
+                $('#confPasswordToggleSignup').removeClass('fa-eye');
+                $('#confPasswordToggleSignup').addClass('fa-eye-slash');
+            } else {
+                $('#inputConfPasswordSignup').attr('type', 'password');
+                $('#confPasswordToggleSignup').removeClass('fa-eye-slash');
+                $('#confPasswordToggleSignup').addClass('fa-eye');
+            }
+        });
+    </script>
+
+</body>
+
+</html>
